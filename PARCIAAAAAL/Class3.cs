46f190da0f675916338e5e0b3d1567ee8f1a69cf
Ayepes2402 <andrea.yepese@upb.edu.co>
2026-03-16 22:34:07 -0500

    Refactor Jugador class methods for item management
    Fix logic errors in GastarOro and AgregarItem methods.

--- PARCIAAAAAL/Class3.cs
+++ PARCIAAAAAL/Class3.cs
@@ -1,6 +1,40 @@
-﻿﻿using System;
+using System;
 
 public class Jugador
 {
-  
+    public decimal Oro { get; private set; }
+
+    public List<Item> Equipamiento { get; } = new List<Item>();
+    public List<Item> Consumibles { get; } = new List<Item>();
+
+    public Jugador(decimal oro)
+    {
+        Oro = oro >= 0 ? oro : 0;
+    }
+
+    public bool PuedePagar(decimal monto)
+    {
+        return monto >= 0 && Oro >= monto;
+    }
+
+    public bool GastarOro(decimal monto)
+    {
+        if (monto < 0  monto > Oro)
+            return false;
+
+        Oro -= monto;
+        return true;
+    }
+
+    public void AgregarItem(Item item)
+    {
+        if (item == null 
+ !item.EsValido())
+            return;
+
+        if (item.Categoria == CategoriaItem.Supply)
+            Consumibles.Add(item);
+        else
+            Equipamiento.Add(item);
+    }
 }
