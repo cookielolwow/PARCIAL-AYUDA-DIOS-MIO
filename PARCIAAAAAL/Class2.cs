457c504f7f102927976e46d329714612c69f7f5b
Ayepes2402 <andrea.yepese@upb.edu.co>
2026-03-16 22:34:23 -0500

    Refactor ItemTienda class constructor and methods
    Fix constructor logic to validate item and cantidad.

--- PARCIAAAAAL/Class2.cs
+++ PARCIAAAAAL/Class2.cs
@@ -1,4 +1,36 @@
-﻿public class ItemTienda
+public class ItemTienda
 {
-   
+    public Item Item { get; }
+    public int Cantidad { get; private set; }
+
+    public ItemTienda(Item item, int cantidad)
+    {
+        if (item == null  !item.EsValido() 
+ cantidad < 0)
+        {
+            Item = item;
+            Cantidad = 0;
+            return;
+        }
+
+        Item = item;
+        Cantidad = cantidad;
+    }
+
+    public bool AgregarCantidad(int cantidad)
+    {
+        if (cantidad < 0) return false;
+
+        Cantidad += cantidad;
+        return true;
+    }
+
+    public bool ReducirCantidad(int cantidad)
+    {
+        if (cantidad < 0 || cantidad > Cantidad)
+            return false;
+
+        Cantidad -= cantidad;
+        return true;
+    }
 }
